e6d98314ffa9950066adfa410b1ff22bd30aeff7
Michaeldz36 <olesik.michael@gmail.com>
2021-02-22 16:15:01 +0100

    made analytical solutions class

--- research/analytical_curves.py
+++ research/analytical_curves.py
@@ -3,78 +3,79 @@ import matplotlib.pyplot as plt
 from utils.utils import Setup
 from utils.random import MP_evals
 
-setup = Setup()
 
-### Generating random eigenvalues
-N=300
+class AnalyticalSolution():
+    def __init__(self, N, P1, P2, tau, epochs1, epochs2, sgm_e, sgm_w1, sgm_w2, sgm_w0, weights_correlation):
+        ### Hyperparameters
+        self.tau = tau
+        self.epochs1 = epochs1
+        self.epochs2 = epochs2
+        self.sgm_e = sgm_e
+        self.sgm_w1 = sgm_w1
+        self.sgm_w2 = sgm_w2
+        self.sgm_w0 = sgm_w0
+        self.weights_correlation = weights_correlation
 
+        ### Generating random eigenvalues
+        self.N=N
+        self.P1=P1
+        self.eVals1 = MP_evals(self.P1,self.N)
+        assert len(self.eVals1)==self.N
 
-P1=300
-eVals1 = MP_evals(P1,N)
-assert len(eVals1)==N
-
-P2=500
-eVals2 = MP_evals(P2,N)
-assert len(eVals2)==N
-
-### Hyperparameters
-tau = 1
-epochs1 = 50
-epochs2 = 50
-sgm_e = setup.sgm_e
-sgm_w1 = setup.sgm_w * 1
-sgm_w2 = setup.sgm_w * 2
-sgm_w0 = 0
-weights_correlation = 0
-
-def E_g11(t):
-    A0 = (sgm_w1**2 + sgm_w0**2)
-    A = lambda t: A0*np.exp(-2 * eVals1* t/tau)
-    B0= 1/eVals1 * sgm_e**2
-    B = lambda t: B0*(1 - np.exp(-eVals1 * t/tau))**2
-    return 1 / N * np.sum(A(t) + B(t)) + sgm_e**2
+        self.P2=P2
+        self.eVals2 = MP_evals(self.P2,self.N)
+        assert len(self.eVals2)==self.N
 
 
-def E_g22(t):
-    A0 = (sgm_w2**2 + sgm_w0**2 * np.exp(-2*eVals1*epochs1/tau)+
-         (sgm_w1**2 + 1 / eVals1 * sgm_e**2)*(1-np.exp(-eVals1*epochs1/tau)**2))
-    A = lambda t: A0 * np.exp(-2*eVals2*(t-epochs1)/tau)
-    B = lambda t: 1/ eVals2 * sgm_e**2 *(1 - np.exp(-eVals2*(t-epochs1)/tau))**2
-    return 1 / N * np.sum(A(t) + B(t)) + sgm_e**2
+
+    def E_g11(self,t):
+        A0 = (self.sgm_w1**2 + self.sgm_w0**2)
+        A = lambda t: A0*np.exp(-2 * self.eVals1* t/self.tau)
+        B0= 1/self.eVals1 * self.sgm_e**2
+        B = lambda t: B0*(1 - np.exp(-self.eVals1 * t/self.tau))**2
+        return 1 / self.N * np.sum(A(t) + B(t)) + self.sgm_e**2
 
 
-def E_g12(t):
-    A = sgm_e ** 2 + sgm_w1 ** 2
-    B = lambda t: sgm_w0 ** 2 * 1 / N * np.sum(np.exp(-2 * eVals1 * epochs1 / tau) * np.exp(-2 * eVals2 * (t - epochs1) / tau))
-    C = lambda t: 1 / N * np.sum((sgm_w1 ** 2 + 1 / eVals1 * sgm_e ** 2) * (1 - np.exp(-eVals1 * epochs1 / tau)) ** 2 * np.exp(
-        -2 * eVals2 * (t - epochs1) / tau))
-    D = lambda t:  1 / N * np.sum((sgm_w2 ** 2 + 1 / eVals2 * sgm_e ** 2) * (1 - np.exp(-eVals2 * (t - epochs1) / tau)) ** 2)
-    E = lambda t: - 2 / N**2 * np.sum((1-np.exp(-eVals2*(t-epochs1)/tau)) * weights_correlation)
-    return A+B(t)+C(t)+D(t)+E(t)
+    def E_g22(self,t):
+        A0 = (self.sgm_w2**2 + self.sgm_w0**2 * np.exp(-2*self.eVals1*self.epochs1/self.tau)+
+             (self.sgm_w1**2 + 1 / self.eVals1 * self.sgm_e**2)*(1-np.exp(-self.eVals1*self.epochs1/self.tau)**2))
+        A = lambda t: A0 * np.exp(-2*self.eVals2*(t-self.epochs1)/self.tau)
+        B = lambda t: 1/ self.eVals2 * self.sgm_e**2 *(1 - np.exp(-self.eVals2*(t-self.epochs1)/self.tau))**2
+        return 1 / self.N * np.sum(A(t) + B(t)) + self.sgm_e**2
 
 
-def make_plot(epochs1=50, epochs2=50, cross_gen=False):
-    timesteps = np.linspace(0, epochs1, 150)
-    e_g11 = np.array([E_g11(_t) for _t in timesteps])
-    plt.plot(timesteps, e_g11 / sgm_w1 ** 2, label="Generalization Error (11")
+    def E_g12(self, t):
+        A0 = np.exp(-2 * self.eVals1 * self.epochs1 / self.tau)
+        A = lambda t: self.sgm_w0 ** 2 * 1 / self.N * np.sum(A0 * np.exp(-2 * self.eVals2 * (t - self.epochs1) / self.tau))
+        B0 = (self.sgm_w1 ** 2 + 1 / self.eVals1 * self.sgm_e ** 2)* (1 - np.exp(-self.eVals1 * self.epochs1 / self.tau)) ** 2
+        B = lambda t: 1 / self.N * np.sum(B0  * np.exp(-2 * self.eVals2 * (t - self.epochs1) / self.tau))
+        C0 = (self.sgm_w2 ** 2 + 1 / self.eVals2 * self.sgm_e ** 2)
+        C = lambda t:  1 / self.N * np.sum(C0 * (1 - np.exp(-self.eVals2 * (t - self.epochs1) / self.tau)) ** 2)
+        D = lambda t: - 2 / self.N**2 * np.sum((1-np.exp(-self.eVals2*(t-self.epochs1)/self.tau)) * self.weights_correlation)
+        return A(t)+B(t)+C(t)+D(t) + self.sgm_e ** 2 + self.sgm_w1 ** 2
 
-    if epochs2:
-        timesteps2 = np.linspace(epochs1, epochs1 + epochs2, 150)
-        e_g22 = np.array([E_g22(_t) for _t in timesteps2])
-        plt.plot(timesteps2, e_g22 / sgm_w2 ** 2, label="Generalization Error (22)")
-        if cross_gen:
-            e_g12 = np.array([E_g12(_t) for _t in timesteps2])
-            plt.plot(timesteps2, e_g12 / sgm_w2 ** 2, label="Generalization Error (12)")
+    def curves(self, timesteps1, timesteps2):
+        e_g11 = np.array([AnalyticalSolution.E_g11(self,_t) for _t in timesteps1])
+        e_g22 = np.array([AnalyticalSolution.E_g22(self,_t) for _t in timesteps2])
+        e_g12 = np.array([AnalyticalSolution.E_g12(self,_t) for _t in timesteps2])
+        return e_g11, e_g22, e_g12
 
+
+
+
+def make_plot(e_g11, e_g22, e_g12, timesteps1, timesteps2):
+    plt.plot(timesteps1, e_g11, label="Generalization Error (11")
+    plt.plot(timesteps2, e_g22, label="Generalization Error (22)")
+    plt.plot(timesteps2, e_g12, label="Generalization Error (12)")
     plt.xlabel("Time")
     plt.ylabel("Generalization Error")
     textstr = '\n'.join((
-        r'$N=%.2f$' % (N,),
-        r'$P_{1}=%.2f$' % (P1,),
-        r'$P_{2}=%.2f$' % (P2,),
-        r'$\sigma_{w_1}=%.2f$' % (sgm_w1,),
-        r'$\sigma_{w_2}=%.2f$' % (sgm_w1,),
-        r'$\sigma_{\epsilon}=%.2f$' % (sgm_e,),
+        r'$N=%.2f$' % (curves.N,),
+        r'$P_{1}=%.2f$' % (curves.P1,),
+        r'$P_{2}=%.2f$' % (curves.P2,),
+        r'$\sigma_{w_1}=%.2f$' % (curves.sgm_w1,),
+        r'$\sigma_{w_2}=%.2f$' % (curves.sgm_w2,),
+        r'$\sigma_{\epsilon}=%.2f$' % (curves.sgm_e,),
     ))
     plt.gcf().text(0.91, 0.12, textstr, fontsize=5)
     plt.grid(True)
@@ -84,7 +85,17 @@ def make_plot(epochs1=50, epochs2=50, cross_gen=False):
     print("Done!")
 
 if __name__ == '__main__':
-    make_plot(epochs1=50, epochs2=70, cross_gen=True)
+    setup = Setup()
+    curves = AnalyticalSolution(N=300, P1=300, P2=500, tau=1,
+                                epochs1=50, epochs2=50, sgm_e=setup.sgm_e,
+                                sgm_w1=setup.sgm_w, sgm_w2=setup.sgm_w * 2,
+                                sgm_w0=setup.sgm_w0, weights_correlation=0)
+    timesteps1 = np.linspace(0, curves.epochs1, 150)
+    timesteps2 = np.linspace(curves.epochs1, curves.epochs1 + curves.epochs2,150)
+    e_g11, e_g22, e_g12 = curves.curves(timesteps1, timesteps2)
+    plt.title("Analytical solutions to 2 linear teacher scenario")
+    make_plot(e_g11/curves.sgm_w1**2, e_g22/curves.sgm_w2**2, e_g12/curves.sgm_w2**2, timesteps1, timesteps2)
+
 
 
 
