b7e9ba9e31682cdbef758a8ec44abdfbf3769ad0
Michaeldz36 <olesik.michael@gmail.com>
2021-02-22 14:28:35 +0100

    added analytical plot for cross generalization error E_g(12)

--- research/analytical_curves.py
+++ research/analytical_curves.py
@@ -25,7 +25,7 @@ sgm_e = setup.sgm_e
 sgm_w1 = setup.sgm_w * 1
 sgm_w2 = setup.sgm_w * 2
 sgm_w0 = 0
-
+w1, w2 = 1, 1
 
 E_g11 = lambda t: 1 / N * np.sum( (sgm_w1**2 + sgm_w0**2)*np.exp(-2 * eVals1* t/tau)
                                   +1/eVals1 * sgm_e**2 * (1 - np.exp(-eVals1 * t/tau))**2 ) + sgm_e**2
@@ -36,23 +36,39 @@ E_g22 = lambda t: 1 / N * \
          (sgm_w1**2 + 1 / eVals1 * sgm_e**2)*(1-np.exp(-eVals1*epochs1/tau)**2))*np.exp(-2*eVals2*(t-epochs1)/tau) +
          1/ eVals2 * sgm_e**2 *(1 - np.exp(-eVals2*(t-epochs1)/tau))**2) + sgm_e**2
 
-if __name__ == '__main__':
-    timesteps = np.linspace(0,epochs1,150)
+
+E_g12 = lambda t: sgm_e**2 +sgm_w1**2 + \
+    sgm_w0**2 * 1 / N * np.sum(np.exp(-2 * eVals1*epochs1/tau)*np.exp(-2 * eVals2*(t-epochs1)/tau)) + \
+    1 / N * np.sum((sgm_w1**2 + 1 /eVals1 * sgm_e**2)*(1-np.exp(-eVals1*epochs1/tau))**2 * np.exp(-2*eVals2*(t-epochs1)/tau)) + \
+    1 / N * np.sum((sgm_w2**2 + 1 /eVals2 * sgm_e**2)*(1-np.exp(-eVals2*(t-epochs1)/tau))**2)
+#     - 2 / N**2 * np.sum((1-np.exp(-eVals2*(t-epochs1)/tau))* 0 #np.sum(w1 * w2))
+
+
+def make_plot(epochs1=50, epochs2=50, cross_gen=False):
+    timesteps = np.linspace(0, epochs1, 150)
     e_g11 = np.array([E_g11(_t) for _t in timesteps])
-    plt.plot(timesteps, e_g11/sgm_w1**2, label="Generalization Error (11")
+    plt.plot(timesteps, e_g11 / sgm_w1 ** 2, label="Generalization Error (11")
+
+    if epochs2:
+        timesteps2 = np.linspace(epochs1, epochs1 + epochs2, 150)
+        e_g22 = np.array([E_g22(_t) for _t in timesteps2])
+        plt.plot(timesteps2, e_g22 / sgm_w2 ** 2, label="Generalization Error (22)")
+        if cross_gen:
+            e_g12 = np.array([E_g12(_t) for _t in timesteps2])
+            plt.plot(timesteps2, e_g12 / sgm_w2 ** 2, label="Generalization Error (12)")
+
     plt.xlabel("Time")
     plt.ylabel("Generalization Error")
     plt.grid(True)
+    # plt.ylim([0,2])
     plt.legend()
     plt.show()
     print("Done!")
 
-    timesteps2 = np.linspace(epochs1, epochs1+epochs2, 150)
-    e_g22 = np.array([E_g22(_t) for _t in timesteps2])
-    plt.plot(timesteps2, e_g22 / sgm_w2 ** 2, label="Generalization Error (22)")
-    plt.xlabel("Time")
-    plt.ylabel("Generalization Error")
-    plt.grid(True)
-    plt.legend()
-    plt.show()
-    print("Done!")
+if __name__ == '__main__':
+    make_plot(epochs1=50, epochs2=50, cross_gen=True)
+
+
+
+
+
