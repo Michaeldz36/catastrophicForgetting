fixed normalisation of produced errors in singla batch setup

research/main.py
@@ -13,18 +13,19 @@ from analytical_curves import AnalyticalSolution
 setup = Setup()
 
 ### Hyperparameters
-N = 30
-P1 = 30
-P2 = 30
+N = 300
+P1 = 300
+P2 = 300
 
 sgm_w1 = setup.sgm_w * 1
 sgm_w2 = setup.sgm_w * 2
 sgm_e = setup.sgm_e
 
-sgm_w0 = 0. #TODO: 0 in article..
+sgm_w0 = 1e-2 ### 0 in article..
+sparsity=1 ### this hack enables us to initialize with 0 weights
 
-epochs1 = 1000
-epochs2 = 0
+epochs1 = 250
+epochs2 = 250
 
 batch_size=P1
 lr = 1e-2 # TODO: simulation strongly dependent on lr...
@@ -44,7 +45,7 @@ def main(N=N, P1=P1, P2=P2, epochs1=epochs1, epochs2=epochs2, sgm_e=sgm_e, sgm_w
     X1_train, X1_test, Y1_train, Y1_test = train_test_split(X1, Y1, test_size=0.33, random_state=42)
     X2_train, X2_test, Y2_train, Y2_test = train_test_split(X2, Y2, test_size=0.33, random_state=42)
 
-    model = Student(n_features=N, sgm_w0=sgm_w0, sparsity=1, depth = d)
+    model = Student(n_features=N, sgm_w0=sgm_w0, sparsity=sparsity, depth = d)
     optimizer = optim.SGD(model.parameters(), lr=lr)
     criterion = nn.MSELoss()
 
@@ -144,8 +145,8 @@ def plot_history(errors, n_runs, variances=None, analytical=False):
 
 if __name__ == '__main__': #TODO: update jupyter notebook
     syllabus = [N, P1, P2, epochs1, epochs2, sgm_w1, sgm_w2, sgm_e, lr, depth]
-    n_runs = 1
+    n_runs = 20
     errors, variances = simulate(syllabus, n_runs)
     plot_history(errors=errors, n_runs=n_runs,
-                 variances=None, analytical=False)
+                 variances=None, analytical=True)
 

utils/utils.py
@@ -92,51 +92,52 @@ def load_data(train_ds, valid_ds=None, generalize_ds=None, batch_size=1):
 def train_valid_loop(data_loaders, data_lengths, n_epochs,
                      optimizer, model, criterion, e_print=1):
     history={"E_train":[], "E_valid":[], "E_cross_g":[]}
-    if n_epochs==0:
+    if n_epochs == 0:
         return history
     for epoch in range(1, n_epochs + 1):
         if epoch % e_print == 0:
             print('Epoch {}/{}'.format(epoch, n_epochs))
             print('-' * 10)
-        # Each epoch has a training and validation phase
+        ### Each epoch has a training and validation phase
         phases = ['train', 'val', 'cross_gen']
         for phase in phases:
             if phase == 'train':
-                model.train(True)  # Set model to training mode
+                model.train(True)  ### Set model to training mode
             else:
-                model.train(False)  # Set model to evaluate mode
+                model.train(False)  ### Set model to evaluate mode
 
             running_loss = 0.0
 
-            # Iterate over data.
+            ### Iterate over data.
             for (Xb, yb) in data_loaders[phase]:
-                # get the input Xs and their corresponding Ys
+                ### get the input Xs and their corresponding Ys
                 X = Xb
                 Y_true = torch.reshape(yb, (yb.shape[0], 1))
 
-                # forward pass to get outputs
+                ### forward pass to get outputs
                 y_pred = model(X)
                 # print("Y_true ", Y_true)
                 # print("y_pred ", y_pred)
                 # print(Y_true.shape, y_pred.shape)
 
-                # calculate the loss between predicted and target
+                ### calculate the loss between predicted and target
                 loss = criterion(y_pred, Y_true)
                 if loss.item()==float("inf"):
                     print("Possibly too large lr!")
                     raise ValueError
-                # zero the parameter (weight) gradients
+                ### zero the parameter (weight) gradients
                 optimizer.zero_grad()
 
-                # backward + optimize only if in training phase
+                ### backward + optimize only if in training phase
                 if phase == 'train':
                     loss.backward()
                     # update the weights
                     optimizer.step()
 
-                # print loss statistics
+                ### update running loss
                 running_loss += loss.item()
-            epoch_loss = running_loss / data_lengths[phase]
+            # TODO!!!! proper normalisation for batch learning!!!!!
+            epoch_loss = running_loss # / data_lengths[phase]
             if phase == 'train':
                 history["E_train"].append(epoch_loss)
             elif phase == 'val':